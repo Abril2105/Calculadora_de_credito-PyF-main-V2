e7b6de0440058cd0fac348823b8f805d9b41d79e
Cylde Cryo <cyldecryo@gmail.com>
2023-11-10 18:50:57 -0500

    areglo de commas

--- Calculadora_de_credito-PyF-main-V2/Calculadora_de_credito-PyF-main/src/components/Calculadora.jsx
+++ Calculadora_de_credito-PyF-main-V2/Calculadora_de_credito-PyF-main/src/components/Calculadora.jsx
@@ -37,7 +37,7 @@ const Calculadora = () => {
       // Reunir la parte entera y la parte decimal en el resultado final
       const result = decimalPart ? formattedIntegerPart + ',' + decimalPart : formattedIntegerPart;
 
-      stateSetter(text);
+      stateSetter(result);
     }
   };
 
